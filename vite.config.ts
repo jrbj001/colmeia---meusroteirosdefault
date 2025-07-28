c37505ebebfbef27c156279fbb61df0a810de0aa
Jose Roberto Baptista junior <jroberto@MacBook-Air-de-Jose.local>
2025-07-28 11:53:50 -0300

    versao mapa funcionado local

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,14 +7,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html')
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
   },
   server: {
     port: 3000,
