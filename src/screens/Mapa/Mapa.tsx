05adfb307e85cfb9a2e4bfde92983480d3d2b72e
Jose Roberto Baptista junior <jroberto@MacBook-Air-de-Jose.local>
2025-09-02 23:23:14 -0300

    feat: alterar estilo do mapa para CartoDB positron
    - Substituir tiles OpenStreetMap por CartoDB positron
    - Atualizar URL dos tiles para https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png
    - Ajustar atribuição para incluir OpenStreetMap e CARTO
    - Melhorar visualização com estilo mais limpo e profissional

--- src/screens/Mapa/Mapa.tsx
+++ src/screens/Mapa/Mapa.tsx
@@ -726,8 +726,8 @@ export const Mapa: React.FC = () => {
                 style={{ height: '100%', width: '100%' }}
               >
                 <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution="&copy; OpenStreetMap contributors"
+                  url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
                 />
                 <AjustarMapa hexagonos={hexagonos} />
                 {hexagonos.map((hex, idx) => (
